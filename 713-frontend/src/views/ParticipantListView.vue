95b53f50d98e97c81a926e6dd47e155520ebad83
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-09 22:04:25 +0700

    Added ParticipantService to handle API requests, Fetched participant data from API and stored in participants array

--- 713-frontend/src/views/ParticipantListView.vue
+++ 713-frontend/src/views/ParticipantListView.vue
@@ -1,86 +1,16 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import ParticipantCard from '@/components/ParticipantCard.vue'
+import ParticipantService from '@/services/ParticipantService'
+import type { Participant } from '@/types'
 
-const participants = ref([
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'j.doe@gmail.com',
-    events: [
-      {
-        id: 1,
-        category: 'Conference',
-        title: 'Tech Conference 2025',
-        description: 'A conference about the latest in tech.',
-        location: 'New York',
-        date: '2025-05-20',
-        time: '10:00 AM',
-        petsAllowed: false,
-        organizer: {
-          id: 1,
-          name: 'Tech Corp',
-        },
-      },
-      {
-        id: 2,
-        category: 'Workshop',
-        title: 'Vue.js Workshop',
-        description: 'A workshop on Vue.js for beginners.',
-        location: 'San Francisco',
-        date: '2025-06-15',
-        time: '2:00 PM',
-        petsAllowed: true,
-        organizer: {
-          id: 2,
-          name: 'Vue Masters',
-        },
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: 'Jane Smith',
-    email: 'j.smith@gmail.com',
-    events: [
-      {
-        id: 3,
-        category: 'Seminar',
-        title: 'AI Seminar 2025',
-        description: 'A seminar about the advancements in AI.',
-        location: 'Los Angeles',
-        date: '2025-07-10',
-        time: '9:00 AM',
-        petsAllowed: false,
-        organizer: {
-          id: 3,
-          name: 'AI Innovators',
-        },
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: 'Alice Johnson',
-    email: 'alice.johnson@gmail.com',
-    events: [
-      {
-        id: 4,
-        category: 'Meetup',
-        title: 'Tech Meetup 2025',
-        description: 'A meetup for tech enthusiasts.',
-        location: 'Chicago',
-        date: '2025-08-05',
-        time: '6:00 PM',
-        petsAllowed: true,
-        organizer: {
-          id: 4,
-          name: 'Tech Enthusiasts',
-        },
-      },
-    ],
-  },
-])
+const participants = ref<Participant[]>([])
+interface PartitipantResponse {
+  data: Participant[]
+}
+ParticipantService.getParticipants().then((response: PartitipantResponse) => {
+  participants.value = response.data
+})
 </script>
 
 <template>
