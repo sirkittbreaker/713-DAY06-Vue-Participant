22b70e26ed18c716f11ac491c49e20b1cb1bdc26
sirkittbreaker <kittienan.kitkiki@gmail.com>
2025-03-09 20:23:42 +0700

    Created a list of participants using ref and used v-for directive to render ParticipantCard for each participant

--- 713-frontend/src/views/ParticipantListView.vue
+++ 713-frontend/src/views/ParticipantListView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import ParticipantCard from '@/components/ParticipantCard.vue'
+
+const participants = ref([
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jane Doe' },
+  { id: 3, name: 'Alice' },
+  { id: 4, name: 'Bob' },
+])
 </script>
 
 <template>
   <div>
-    <ParticipantCard />
+    <ParticipantCard v-for="participant in participants" :key="participant.id" />
   </div>
 </template>
